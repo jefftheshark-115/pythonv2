912ef38e1284978b320d0dcf90df1ed8202c3cae
Shane Frady <shanefrady42@gmail.com>
2026-09-02 14:40:04 -0500

    123

--- Domain 2/Student/227-analyze.py
+++ Domain 2/Student/227-analyze.py
@@ -2,5 +2,6 @@ operands = ['+','-','*','/','//','%']
 for operand in operands:
     if operand == '//':
         print('// is a floor division operand')
+        #break
         continue
     print(operand, 'is an operand')
--- Domain 2/Student/227-compound.py
+++ Domain 2/Student/227-compound.py
@@ -2,7 +2,7 @@ coins = ('Bronze','Silver','Platinum','Gold')
 scepter = False
 for coin in coins:
 
-    if coin == 'Platinum':
+    if coin == 'Platinum' and scepter == True:
         print('Congratulations! The platinum coin will move you to the next level!')
         continue
     print ('You possess a', coin, 'coin.')
