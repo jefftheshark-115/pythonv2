58c6933541806de549070bebd90a18eb4f317b2a
Shane Frady <shanefrady42@gmail.com>
2026-09-22 13:48:58 -0500

    idk

--- Domain 6/Student/611-io.py
+++ Domain 6/Student/611-io.py
@@ -5,5 +5,5 @@ game_stream = io.StringIO()
 game_stream.write("The game has started.\n")
 game_stream.write("Here is your first question. \n")
 
-game_stream.seek(0)
+game_stream.seek(1)
 print(game_stream.read())
--- Domain 6/Student/623-final.py
+++ Domain 6/Student/623-final.py
@@ -1,9 +1,10 @@
 import random
+operand = random.randint(1,3)
+print("this is your operand", operand, "\n" "if it's one then addition" "\n" "if it's two then subtraction " "\n" "if it's three then multiplication")
 number1 = int(input('Enter your first number '))
 number2 = int(input('Enter your second number '))
 
 def calculate(number1, number2):
-    operand = random.randint(1,3)
     if operand == 1:
         operator = "+"
         print(number1, "+", number2, "=", number1 + number2)
